5737ff2e9e21d556e35fb952f0e47b082fc76534
Arcturus <46330334+MogsFriend@users.noreply.github.com>
2022-11-21 14:57:55 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+using System.Drawing.Imaging;
+
+Console.WriteLine("Hello, World!");
+
+bool dstdir = false;
+bool srcdir = false;
+
+if (Directory.Exists("source")) srcdir = true;
+if (!srcdir) Directory.CreateDirectory("source");
+
+if (Directory.Exists("destination")) dstdir = true;
+if (!dstdir) Directory.CreateDirectory("destination");
+
+if (File.Exists("mask.png"))
+{
+    Bitmap maskimage = (Bitmap)Image.FromFile("mask.png");
+    string[] files = Directory.GetFiles("source", "*.png");
+    foreach(string file in files)
+    {
+        string filename = Path.GetFileName(file);
+        if (!File.Exists(Path.Combine("destination", filename)))
+        {
+            Console.Write($"{filename} Working ...");
+            Bitmap target = (Bitmap)Image.FromFile(file);
+
+            for (int x = 0; x < maskimage.Width; x++)
+            {
+                for (int y = 0; y < maskimage.Height; y++)
+                {
+                    if (target.Width > x && target.Height > y)
+                    {
+                        var c = target.GetPixel(x, y);
+                        target.SetPixel(x, y, Color.FromArgb(maskimage.GetPixel(x, y).A, c.R, c.G, c.B));
+                    }
+                }
+            }
+            target.Save(Path.Combine("destination", filename), ImageFormat.Png);
+            Console.WriteLine($" OK");
+        }
+        else
+        {
+            Console.WriteLine($" Skip");
+        }
+    }
+}
